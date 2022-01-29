ddec03cc5ddea27701882c521e5f9203062380fa
KdotA <kenan@akinci.io>
2022-01-30 00:56:58 +0100

    API call test

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,16 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+
+    const data = await res.json();
+    console.log(res, data);
+  } catch (err) {
+    alert(err);
+  }
+};
